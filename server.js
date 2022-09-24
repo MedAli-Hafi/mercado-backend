23207f1e0006e85b82c1cc1f0307e288edd2257a
Mohamed Ali Hafi <mohamed.ali.hafi.contact@gmail.com>
2022-09-24 12:15:32 +0100

    Add WEB_APP_URL as an environment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: [ "http://localhost:3000" ] }));
+app.use(cors({ credentials: true, origin: [process.env.WEB_APP_URL] }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -21,12 +21,12 @@ app.use('/auth', usersRouter);
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-        .then(() => {
-            console.log('Successfully connected to MongoDB');
-            app.listen(PORT, () => {
-                console.log(`Server is listening on port ${PORT}`);
-            });
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    .then(() => {
+        console.log('Successfully connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
